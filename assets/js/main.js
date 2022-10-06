f22b04c3838d7bd926713b9175352881593fe58f
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-10-06 19:22:26 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//? Duration: property- counts number of seconds between calling start method and calling stop method
+//*timeEnd: method starts a timer with the specified label. A subsequent call to the console. timeEnd() method with the same label will output milliseconds elapsed since the first call was made.
+//? Stop: method- stops the timer, can't be stopped if not started
+//? Start: method- starts the timer, can't be stopped if not stopped
+//? reset: method- takes stopwatch to inital state
+//@typescript
+
